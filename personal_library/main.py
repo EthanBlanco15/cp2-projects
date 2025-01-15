241fbed590cf67f6caf6ec4188bf2c19751e4f7b
EthanBlanco15 <ethan.blanco@ucas-edu.net>
2025-01-15 10:51:56 -0700

    Started the main interface

---
+++ personal_library/main.py
@@ -0,0 +1,14 @@
+#Ethan Blanco, Personal Library Program
+
+artists = []
+
+def main(): #Main user interface
+
+    while True:
+        mus_fes = input("Welcome to the Music Festival, are you part of the audience or a managing leader?\n")
+        print("Press 1 to watch the show as an audience member.")
+        print("Press 2 to watch the show as a management leader.")
+        print("Press 3 to leave.")
+        pass
+
+main()
