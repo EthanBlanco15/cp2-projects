80520968e03900d9a40f8ba531b37a15dbf457e4
EthanBlanco15 <ethan.blanco@ucas-edu.net>
2025-01-17 10:52:01 -0700

    finish functions

--- personal_library/main.py
+++ personal_library/main.py
@@ -1,20 +1,25 @@
 #Ethan Blanco, Personal Library Program
 
-library = ["Harry Potter, by J. K. Rowling, comedy"]
+library = {"Harry Potter, by J. K. Rowling", "Wonder, R. J. Palacio", "The Amazing Spider-Man, Stan Lee"}
+genre = ("Harry Potter is a comedy book about wizardry", "Wonder is a fiction book about individuality", "The Amazing Spider-Man is an action book about balance")
 
-def add(): #This function is supposed to allow the user to add any type of book with the title, author and one type of genre.
+def addition(book_addition, book_genre): #This function is supposed to allow the user to add any type of book with the title, author and one type of genre.
+    book_addition = input("What book would you like to add onto your library? Example; Scythe, Neal Shusterman")
+    library.add(book_addition)
+    book_genre = input("What is that book about and its genre? Example; Scythe is a science fiction book about loss and immortality")
+    library.add(book_genre)
+    print(f"Here's you updated library! {library}")
+    pass
+
+addition()
+
+def removal(book_removal): #The function here allows the user to remove any type of book, removes the title, author, and type of genre by default.
 
     pass
 
-add()
+removal()
 
-def remove(): #The handy dandy function here allows the user to remove any type of book, removes the title, author, and type of genre by default.
-
-    pass
-
-remove()
-
-def search_bar(): #Lets you search for a specific known book by title, author, and genre.
+def search_bar(book_search): #Lets you search for a specific known book by title, author, and genre.
 
     pass
 
@@ -27,18 +32,23 @@ def main(): #Main user interface
         Type 1 to view your library.
         Type 2 to add items into your library.
         Type 3 to remove items from your library.
-        Type 4 to search the details about an item from your library.
+        Type 4 to search up an item from your library.
         Type 5 to leave.\n"""))
         pass
         if pers_libr == 1:
             print(f"Here's your personal library, take a look! {library}")
-        if pers_libr == 2:
+        elif pers_libr == 2:
             pass
-        if pers_libr == 3:
+        elif pers_libr == 3:
             pass
-        if pers_libr == 4:
+        elif pers_libr == 4:
             pass
-        if pers_libr == 5:
-            pass
+        elif pers_libr == 5:
+            print("Thank you for using your personal library, goodbye!")
+            break
+        elif pers_libr == "":
+            print("You wrote nothing, try again.")
+        else:
+            print("This doesn't work, try again.")
 
 main()
