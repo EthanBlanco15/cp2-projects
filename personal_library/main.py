d654b332ed23b43cf2562f1c9b44c0bb06ae60a4
EthanBlanco15 <ethan.blanco@ucas-edu.net>
2025-01-21 10:51:42 -0700

    fin

--- personal_library/main.py
+++ personal_library/main.py
@@ -9,9 +9,7 @@ def addition(book_addition, book_genre): #This function is supposed to allow the
     book_genre = input("What is that book about and its genre? Example; Scythe is a science fiction book about loss and immortality")
     library.add(book_genre)
     print(f"Here's you updated library! {library}")
-    pass
-
-addition()
+    return(addition)
 
 def removal(book_removal): #The function here allows the user to remove any type of book, removes the title, author, and type of genre by default.
 
@@ -38,7 +36,8 @@ def main(): #Main user interface
         if pers_libr == 1:
             print(f"Here's your personal library, take a look! {library}")
         elif pers_libr == 2:
-            pass
+            upd_libr = addition(book_addition=, book_genre=)
+            print("Here's your updated library")
         elif pers_libr == 3:
             pass
         elif pers_libr == 4:
