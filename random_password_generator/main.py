2ada9e23de80cfd43e09e21991f784ea2729c4e5
EthanBlanco15 <ethan.blanco@ucas-edu.net>
2025-01-23 10:51:21 -0700

    started not finished

---
+++ random_password_generator/main.py
@@ -0,0 +1,32 @@
+#Ethan Blanco, Random Password Generator
+
+def upper_letter():
+
+    pass
+
+upper_letter()
+
+def lower_letter():
+
+    pass
+
+lower_letter()
+
+def num():
+
+    pass
+
+num()
+
+def speci_chara():
+
+    pass
+
+speci_chara()
+
+def main():
+
+    while True:
+        pick_passw = input("")
+
+main()
