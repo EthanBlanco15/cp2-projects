dab804fa6560d5ad0929efac3e49fc1a79708235
EthanBlanco15 <ethan.blanco@ucas-edu.net>
2025-01-28 10:49:52 -0700

    bare functions

---
+++ simple_morse_code_translator/main.py
@@ -0,0 +1,42 @@
+#Ethan Blanco
+
+alphab = []
+morsec = ("")
+
+def alphabet(alpha_words): #A function that converts morse code into modern english.
+
+    pass
+
+alphabet()
+
+def morsecode(morse_words): #A function that deals with morsecode understanding.
+
+    pass
+
+morsecode()
+
+def translator(morse_alpha_conversion): #Used for translating the morse code into words
+    
+    pass
+
+translator()
+
+def main(main_userin): #Main user interface that will mostly be seen when starting.
+    
+    while True:
+        main_userin = input("""Welcome to the morse code translator
+                            type yes if you would like to translate words into morse code,
+                            or type no to morse code into words?
+                            and type exit if you'd like to leave the program\n""")
+        if main_userin == "yes" and "Yes" and "YES" and "y" and "Y":
+            pass
+        elif main_userin == "no" and "No" and "NO" and "n" and "N":
+            pass
+        elif main_userin == "exit" and "Exit" and "EXIT":
+            pass
+        else:
+            print("This doesn't work, maybe try again later.")
+            continue
+
+
+main()
