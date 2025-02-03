23913352b0bccba47ce95df1db79ca74fdc828a1
EthanBlanco15 <ethan.blanco@ucas-edu.net>
2025-02-03 10:49:28 -0700

    got morse code dictionary

--- simple_morse_code_translator/main.py
+++ simple_morse_code_translator/main.py
@@ -1,25 +1,34 @@
 #Ethan Blanco
 
 alphab = []
-morsec = ("")
+morsec_dict = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                    '?':'..--..', '/':'-..-.', '-':'-....-',
+                    '(':'-.--.', ')':'-.--.-'}
+morsecode = ""
 
-def alphabet(alpha_words): #A function that converts morse code into modern english.
-
-    pass
-
-alphabet()
-
-def morsecode(morse_words): #A function that deals with morsecode understanding.
-
-    pass
-
-morsecode()
-
-def translator(morse_alpha_conversion): #Used for translating the morse code into words
+def morse_translator(morse_alpha_conversion): #Used for translating the morse code into words.
     
-    pass
+    morse_alpha_conversion = input("What morse code word would you like translated?")
 
-translator()
+morse_translator()
+
+def alpha_translator(alpha_conversion): #Used for translating the English words into morse code.
+
+    alpha_conversion = input("What English worde would you like translated?")
+
+alpha_translator()
 
 def main(main_userin): #Main user interface that will mostly be seen when starting.
     
@@ -28,12 +37,13 @@ def main(main_userin): #Main user interface that will mostly be seen when starti
                             type yes if you would like to translate words into morse code,
                             or type no to morse code into words?
                             and type exit if you'd like to leave the program\n""")
-        if main_userin == "yes" and "Yes" and "YES" and "y" and "Y":
-            pass
-        elif main_userin == "no" and "No" and "NO" and "n" and "N":
-            pass
-        elif main_userin == "exit" and "Exit" and "EXIT":
-            pass
+        if main_userin == "yes" or "Yes" or "YES" or "y" or "Y":
+            alpha_translator#(alpha_conversion)
+        elif main_userin == "no" or "No" or "NO" or "n" or "N":
+            morse_translator#(morse_alpha_conversion)
+        elif main_userin == "exit" or "Exit" or "EXIT":
+            print("Goodbye!")
+            break
         else:
             print("This doesn't work, maybe try again later.")
             continue
