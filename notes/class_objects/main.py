09e58160add28962743f11e192f768c01a4602eb
EthanBlanco15 <ethan.blanco@ucas-edu.net>
2025-04-23 10:26:02 -0600

    saving notes

--- notes/class_objects/main.py
+++ notes/class_objects/main.py
@@ -1,6 +1,7 @@
 #Classes and Objects notes
 
-#Class is a blueprint for creating an object
+# What is a class in Python?
+# -- A class is a blueprint for creating an object.
 class pokemon:
     def __init__(self, name, species, hp, dmg):
         self.name = name
@@ -9,7 +10,7 @@ class pokemon:
         self.dmg = dmg
 
     def __str__(self):
-        return f"Name: {self.name}\nSpecies: {self.species}\nHeatlh: {self.hp}\n Attack: {self.dmg}"
+        return f"Name: {self.name}\nSpecies: {self.species}\nHealth: {self.hp}\n Attack {self.dmg}"
 
     def battle(self, opponent):
         while self.hp > 0 and opponent.hp > 0:
@@ -17,16 +18,38 @@ class pokemon:
             print(f"{self.name} attacked {opponent.name} for {self.dmg} and {opponent.name} now has {opponent.hp}")
             if opponent.hp > 0:
                 self.hp -= opponent.dmg
-                print(f"{opponent.name} attacked {self.name} for {opponent.dmg} and {self.name} now has {self.hp}")
+                print(f"{self.name} attacked {opponent.name} for {self.dmg} and {opponent.name} now has {opponent.hp}")
                 if self.hp <= 0:
-                    print(f"{self.name} has been knocked out. {opponent.name} won the battle! ")
+                    print(f"{opponent.name} has been TKO'd {self.name} won the battle!")
             else:
-                print(f"{opponent.name} has been knocked out. {self.name} won the battle!")
+                print(f"{opponent.name} has been TKO'd {self.name} won the battle!")
 
 
 bob = pokemon("Mr.Bob", "Charizard", 300, 95)
-fluffy = pokemon("Fluffy","Arcanine", 280, 110)
+fluffy = pokemon("Fluffy", "Arcanine", 280, 110)
 
-print(bob)
-bob.battle(fluffy)
+print(bob.name)
 print(fluffy.species)
+
+bob.battle(fluffy)
+
+# What is an object in python?
+# -- An object is a specific instance of a class.
+
+# How do python classes relate to python objects?
+# -- A class is the blueprint and the object is the house built from the blueprint.
+
+# How do you create a python class?
+# -- You create a class by using the word "class", then the class name with a colon.
+
+# What are methods?
+# -- A method is a function specific to the class.
+
+# How do you create a python object?
+# -- You can create an object by calling the class, just like a function.
+
+# How to you call a method for an object?
+# -- You can call a method for an object by using a dot notation.
+
+# Why do we use python classes?
+# -- Python classes can help us organize our code better and reuse code.
